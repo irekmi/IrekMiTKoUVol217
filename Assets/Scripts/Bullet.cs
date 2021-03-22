3d31b5bfdb23d300099f6917cc44585b9c219781
irekmi <irekmi37@gmail.com>
2021-03-22 16:56:11 +0100

    NotSure

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    NewBullet newB;
+
+    private bool grounded = false;
+    private bool pointerPressed = false;
+    private float releaseTime = .2f;
+    private float xVelocity;
+
+    private void Start()
+    {
+        newB = GameObject.FindWithTag("Respawn").GetComponent<NewBullet>();
+        grounded = false;
+    }
+    private void Update()
+    {
+        xVelocity = rb.velocity.x;
+        Debug.Log(xVelocity);
+        if (pointerPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+    private void OnMouseDown()
+    {
+        pointerPressed = true;
+    }
+    private void OnMouseUp()
+    {
+        pointerPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(DropThatBall());
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!grounded && xVelocity < .5f && xVelocity > -0.5f)
+        {
+            StartCoroutine(NewBullet());
+            grounded = true;
+        }
+    }
+    IEnumerator DropThatBall()
+    {
+        yield return new WaitForSeconds(releaseTime);
+        GetComponent<SpringJoint2D>().enabled = false;
+    }
+
+    IEnumerator NewBullet()
+    {
+        yield return new WaitForSeconds(1f);
+        newB.NewBall();
+        StopCoroutine(NewBullet());
+    }
+
+}
---
+++ Assets/Scripts/NewBullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBullet : MonoBehaviour
+{
+    public GameObject bullet;
+
+    public void NewBall()
+    {
+        Instantiate(bullet, new Vector3(-5f, -1f, 0f),Quaternion.identity);
+    }
+}
